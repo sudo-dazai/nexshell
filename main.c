8caf711a3b99d2b8d3e6d84560242c12b016ed46
yuta-go <trojans2709@gmail.com>
2024-09-15 10:56:15 +0530

    basic interaction

--- main.c
+++ main.c
@@ -8,6 +8,43 @@
 #define ctrl(x) ((x) & 0x1f)
 #define SHELL "[fnsh]$ "
 #define ENTER 10
+#define DATA_START_CAPACITY 128
+
+  #define ASSERT(cond, ...) \
+        do { \
+            if (!(cond)) { \
+                endwin();   \
+                fprintf(stderr, "%s:%d: ASSERTION FAILED: ", __FILE__, __LINE__); \
+                fprintf(stderr, __VA_ARGS__); \
+                fprintf(stderr, "\n"); \
+                exit(1); \
+            } \
+        } while (0)
+
+    #define DA_APPEND(da, item) do {                                                       \
+        if ((da)->count >= (da)->capacity) {                                               \
+            (da)->capacity = (da)->capacity == 0 ? DATA_START_CAPACITY : (da)->capacity*2; \
+            void *new = calloc(((da)->capacity+1), sizeof(*(da)->data));                   \
+            ASSERT(new,"out of ram");                                                       \
+            if ((da)->data != NULL)                                                        \
+                memcpy(new, (da)->data, (da)->count);                                      \
+            free((da)->data);                                                              \
+            (da)->data = new;                                                              \
+        }                                                                                  \
+        (da)->data[(da)->count++] = (item);                                                \
+    } while (0)
+
+typedef struct {
+  char *data;
+  size_t count;
+  size_t capacity;
+} String;
+
+typedef struct {
+  String *data;
+  size_t count;
+  size_t capacity;
+} Strings;
 
 int main() {
   initscr();		
@@ -17,27 +54,27 @@ int main() {
   bool QUIT = false;
   int in;
 
-  char command[1024] = {0};
-  size_t command_s = 0;
+  String command = {0};
+  Strings command_history = {0};
   size_t line = 0;
 
   while(!QUIT) {
     mvprintw(line, 0 , SHELL);
-    mvprintw(line, 0+sizeof(SHELL)-1,command);
+    mvprintw(line, 0+sizeof(SHELL)-1,"%.*s",(int)command.count, command.data);
     
     in = getch();
     switch(in) {
       case ctrl('q'): 
         QUIT = true;
         break;
-      case KEY_ENTER:
+      //case KEY_ENTER:
       case ENTER:
         line++;
-        memset(command, 0, sizeof(char)* command_s);
-        command_s = 0;
+        DA_APPEND(&command_history, command);
+        command = (String){0};
         break;
-      default: 
-        command[command_s++] = in;
+      default:
+        DA_APPEND(&command, in);
         break;
     }
   }
@@ -45,6 +82,9 @@ int main() {
 
 	refresh();			;		
 	endwin();	
-
+  for(size_t i = 0; i < command_history.count; i++) {
+    printf("%.*s\n", (int)command_history.data[i].count, command_history.data[i].data);
+    free(command_history.data[i].data);
+  }
   return 0;
 }
